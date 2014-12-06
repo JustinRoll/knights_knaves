1456c48d19c306adebd4c70f023f13af231d3d55
Justin Roll <justin.j.roll@gmail.com>
2014-12-06 12:42:12 -0800

    final knights/knaves commit

--- driver.py
+++ driver.py
@@ -16,6 +16,21 @@ def test():
         logic._counter._value = 0
         prover = Prover9()
 
+#assumption: -knight(Zoey) and -knight(Mel) goal: (-(-knight(Mel)) and -knight(Zoey)) and (-(knight(Zoey) and knight(Mel)) and -knight(Mel))
+#assumption: -knight(Zoey) and knight(Mel) goal: ((-knight(Mel)) and knight(Zoey)) and (-(knight(Zoey) and knight(Mel)) and -knight(Mel))
+
+
+
+        logic._counter._value = 0
+        prover = Prover9()
+#
+#assumption: knight(Zoey) and knight(Mel) goal: ((-knight(Mel)) and knight(Zoey)) and ((knight(Zoey) and knight(Mel)) and knight(Mel))
+        lp = LogicParser()  
+        p1 = read_expr('knight(mel) and knight(zoey)') #zoey and mel are knights
+        c  = read_expr('-knight(mel) and ((knight(zoey) and knight(mel)) and knight(mel))') #we are trying to prove that Zoey is a knight, Mel is a knave
+        prover = ResolutionProverCommand(c, [p1])
+        print(prover.prove())
+
         lp = LogicParser()  
         p1 = read_expr('-knight(mel) and -knight(zoey)') #zoey and mel are knaves
         c  = read_expr('knight(mel) and -(knight(zoey) and knight(mel))')
@@ -26,31 +41,18 @@ def test():
         prover = Prover9()
 
         lp = LogicParser()  
-        p1 = read_expr('knight(mel) and knight(zoey)') #zoey and mel are knights
-        c  = read_expr('-knight(mel) and (knight(zoey) and knight(mel))') #we are trying to prove that Zoey is a knight, Mel is a knave
-        prover = ResolutionProverCommand(c, [p1])
-        print(prover.prove())
-
-        logic._counter._value = 0
-        prover = Prover9()
-
-        lp = LogicParser()  
-        p1 = read_expr('-knight(mel) and knight(zoey)') #zoey knight, mel knave
-        c  = read_expr('-knight(mel) and -(knight(zoey) and knight(mel))') #we are trying to prove that Zoey is a knight, Mel is a knave
-        prover = ResolutionProverCommand(c, [p1])
-        print(prover.prove()) 
-
-
-        logic._counter._value = 0
-        prover = Prover9()
-
-        lp = LogicParser()  
         p1 = read_expr('knight(mel) and -knight(zoey)') #zoey knave, mel knight
         c  = read_expr('(knight(mel)) and (knight(zoey) and knight(mel))') #we are trying to prove that Zoey is a knight, Mel is a knave
         #prover = Prover9Command(c, [p1])
         prover = ResolutionProverCommand(c, [p1])
         print(prover.prove())
-        print(prover.proof())
+
+        lp = LogicParser()  
+        p1 = read_expr('-knight(mel) and knight(zoey)') #zoey knight, mel knave
+        c  = read_expr('(knight(zoey) and -knight(mel)) and (-(knight(zoey) and knight(mel)) and -knight(mel))') #we are trying to prove that Zoey is a knight, Mel is a knave
+        prover = ResolutionProverCommand(c, [p1])
+        print(prover.prove()) 
+#assumption: knight(Zoey) and -knight(Mel) goal: (-(-knight(Mel)) and -knight(Zoey)) and ((knight(Zoey) and knight(Mel)) and knight(Mel))
 
 def test2():
 #You meet two inhabitants: Peggy and Zippy. Peggy tells you that 'of Zippy and I, exactly one is a knight'. Zippy tells you that only a knave would say that Peggy is a knave.
@@ -94,4 +96,14 @@ def test2():
         print(prover.prove())
         print(prover.proof()) 
 
-test2()
+def test3():
+        lp = LogicParser()  
+        p1 = read_expr('knight(Zed) and knight(Bart)') #zoey knave, mel knight
+        c  = read_expr("((knight(Bart) or knight(Zed)) and knight(Zed)) and ((-knight(Zed) and knight(Bart)) or (knight(Zed) and -knight(Bart)) and knight(Bart))")  #we are trying to prove that Zoey is a knight, Mel is a knave
+        #prover = Prover9Command(c, [p1])
+        prover = ResolutionProverCommand(c, [p1]) 
+        print(prover.prove())    
+
+
+
+test()
--- sentParserRe.py
+++ sentParserRe.py
@@ -1,12 +1,66 @@
 from puzzleDict import PuzzleInfo
 import re
+import nltk
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
+from nltk.sem.drt import *
+from nltk.sem import logic
+from nltk.sem.logic import *
+from nltk.inference.resolution import *
+read_expr = nltk.sem.Expression.fromstring 
+import sys
 
 class KKLogic:
     def __init__(self):
         self.entities = ()
         self.statements = []
+
+
+    def validateStatement(self, assumptionStr, goalStr):
+        lp = LogicParser()  
+    
+#        print("assumption: %s goal: %s" % (assumptionStr, goalStr))
+        assumption = read_expr(assumptionStr) #zoey and mel are knaves
+        goal  = read_expr(goalStr)
+        prover = ResolutionProverCommand(goal, [assumption])
+        return prover.prove()        
+
+    def evalKnightKnave(self):
+        #person -> knight_knave -> stmt
+        logicDict = {}
+        bothKnights = "knight(%s) and knight(%s)" % (self.entities[0], self.entities[1])
+        bothKnaves = "-knight(%s) and -knight(%s)" % (self.entities[0], self.entities[1])
+        firstKnight = "knight(%s) and -knight(%s)" % (self.entities[0], self.entities[1])
+        secondKnight = "-knight(%s) and knight(%s)" % (self.entities[0], self.entities[1])
+        
+        bothKnightsResult = "%s is a knight. %s is a knight." % (self.entities[0], self.entities[1])
+        bothKnavesResult = "%s is a knave. %s is a knave." % (self.entities[0], self.entities[1])
+        firstKnightResult = "%s is a knight. %s is a knave." % (self.entities[0], self.entities[1])
+        secondKnightResult = "%s is a knave. %s is a knight." % (self.entities[0], self.entities[1])
+        #(-((knight(Ted) and -knight(Zippy)) or (-knight(Ted) and knight(Zippy))) and -knight((knight(Ted) and -knight(Zippy)) or (-knight(Ted) and knight(Zippy)))) and (-(-knight(Ted)) and -knight(Zippy))
+
+        #check possibility that both are knights
+        andedStatements = "((%s) and knight(%s)) and ((%s) and knight(%s))" % (self.statements[0][1], self.statements[0][0], self.statements[1][1], self.statements[1][0])  #need to and in more?
+        kkTrue = self.validateStatement(bothKnights, andedStatements)
+        if kkTrue:
+            return bothKnightsResult
+        #bothKnaves 
+        knaveKnaveTrue = self.validateStatement(bothKnaves, "(-(%s) and -knight(%s)) and (-(%s) and -knight(%s))" % (self.statements[0][1], self.statements[0][0], self.statements[1][1], self.statements[1][0]))
+        if knaveKnaveTrue:
+            return bothKnavesResult
+        #firstKnave
+        firstKnightTrue = self.validateStatement(firstKnight, "((%s) and knight(%s)) and (-(%s) and -knight(%s))" % (self.statements[0][1], self.statements[0][0], self.statements[1][1], self.statements[1][0]))
+        if firstKnightTrue:
+            return firstKnightResult
+
+
+        #secondKnave
+        secondKnightTrue = self.validateStatement(secondKnight, "(-(%s) and -knight(%s)) and ((%s) and knight(%s))" % (self.statements[0][1], self.statements[0][0], self.statements[1][1], self.statements[1][0]))
+        if secondKnightTrue:
+            return secondKnightResult
+       
+        return None    
+
 
 class SentParser:
 
@@ -29,6 +83,7 @@ class SentParser:
         kkLogic = KKLogic()
         for sent in sents:
             self.parseSentence(sent, kkLogic)
+        return kkLogic
 
     def parseSentence(self, sent, kkLogic):
         sentText = sent
@@ -38,7 +93,13 @@ class SentParser:
             self.parseSentLogic(sentText, kkLogic)
     
     def containsNot(self, sent):
-        if "it is false that" in sent or "not the case that" in sent:
+        if "it is false that" in sent or "it's false that" in sent or "not the case that" in sent:
+            return True
+        else:
+            return False
+
+    def containsAreBoth(self, sent):
+        if "are both knights" in sent or "are both knaves" in sent:
             return True
         else:
             return False
@@ -48,6 +109,16 @@ class SentParser:
             return True
         else:
             return False
+
+    def genAreBoth(self, text, kkLogic):
+        regExp = r"are both ((knights)|(knaves))"
+        result = re.search(regExp, text)
+        if result and result.group(1):
+            ty = result.group(1)
+            if ty == "knights":
+                return "knight(%s) and knight(%s)" % (kkLogic.entities[0], kkLogic.entities[1])
+            elif ty == "knaves":
+                return "knave(%s) and knave(%s)" % (kkLogic.entities[0], kkLogic.entities[1]) 
 
     def genBoth(self, kkLogic):
         return "(knight(%s) and knight(%s)) or (knave(%s) and knave(%s))" % (kkLogic.entities[0], kkLogic.entities[1], kkLogic.entities[0], kkLogic.entities[1])
@@ -60,27 +131,42 @@ class SentParser:
         speakerRegExp = "([A-Z][a-z]*) ((tells you that)|(says)|(claims)|(tells you))"
         result = re.search(speakerRegExp, sentText)
         speaker = result.group(1) #always a speaker
-        print(sentText)
+        #print(speaker)
+        #print(sentText)
+        statement = ""
         if self.containsConjunction(sentText):
-            print(self.getConjunctionStatement(sentText, speaker))
+            statement = self.getConjunctionStatement(sentText, speaker, kkLogic)
+            #print(self.getConjunctionStatement(sentText, speaker, kkLogic))
         elif self.containsXor(sentText):
-            print("  " +self.generateXor(sentText, kkLogic))
+            statement = self.generateXor(sentText, kkLogic)
+            #print("  " +self.generateXor(sentText, kkLogic))
         elif self.containsCouldSay(sentText):
-            self.genCouldSay(sentText, speaker, kkLogic)
+            statement = self.genCouldSay(sentText, speaker, kkLogic)
+            #print(self.genCouldSay(sentText, speaker, kkLogic))
         elif self.containsBoth(sentText):
-            print("  " + self.genBoth(kkLogic)) #both knights or both knaves
+            statement = self.genBoth(kkLogic)
+            #print("  " + self.genBoth(kkLogic)) #both knights or both knaves
         elif self.containsOnlyA(sentText):
-            print(self.genOnlyA(sentText))
+            #print(self.genOnlyA(sentText))
+            statement = self.genOnlyA(sentText)
+        elif self.containsAreBoth(sentText):
+           statement = self.genAreBoth(sentText, kkLogic)
+           #print(self.genAreBoth(sentText, kkLogic))
+        elif self.containsNor(sentText):
+            statement = self.genNor(sentText, kkLogic)
+            #print(self.genNor(sentText, kkLogic))
         elif self.containsBasicStatement(sentText):
-            print("  " + self.getBasicStatement(sentText, speaker))
+            statement = self.getBasicStatement(sentText, speaker)
+            #print("  " + self.getBasicStatement(sentText, speaker))
         
         #if baseResult and baseResult.group(1) and baseResult.group(2):                                                                       
            # print(baseResult.group(1) + " " + baseResult.group(2))
         #parse the basic logic, then see if we need to not stuff
         
-        #kkLogic.statements.append(speaker, logic)
+        if statement:
+            kkLogic.statements.append((speaker, statement.replace("knave", "-knight")))
 
-    def getConjunctionStatement(self, sentText, speaker):
+    def getConjunctionStatement(self, sentText, speaker, kkLogic):
         #Either Zed is a knight or I am a knight.
         #At least one of the following is true: that I am a knight or that Bozo is a knight.'
         #`Zoey and I are both knights or both knaves.'
@@ -103,19 +189,23 @@ class SentParser:
         leastExp = r"At least one of the following is true: that (([A-Z])[a-z]*) am a ((knight)|(knave)) ((and)|(or)) that ([A-Z][a-z]*) is a ((knight)|(knave))"
         result = re.search(leastExp, sentText)
         if result and result.groups(3):
-            statement = "%s(%s) %s %s(%s)" % (result.groups(1)[2], result.groups(2)[0], result.groups(3)[5], result.groups(4)[9], result.groups(5)[8])) 
+            statement = "%s(%s) %s %s(%s)" % (result.groups(1)[2], result.groups(2)[0], result.groups(3)[5], result.groups(4)[9], result.groups(5)[8]) 
             return statement
 
 
         #I and marge are knights
         leastExp = r"I and ([A-Z][a-z]*) are ((knights)|(knaves))"
         result = re.search(leastExp, sentText)
-        if result and result.groups(3):
-            statement = "%s(%s) %s %s(%s)" % (result.groups(1)[2], result.groups(2)[0], result.groups(3)[5], result.groups(4)[9], result.groups(5)[8])) 
-            return statement 
+        if result:
+            grp = result.groups()[2][:-1]
+            return "%s(%s) and %s(%s)" % (grp, kkLogic.entities[0], grp, kkLogic.entities[1]) 
 
         #Zed tells you, `I am a knight or Alice is a knave.'
-        leastExp = r"I am a ((knight)|(knave)) or ([A-Z][a-z]+) is a ((knight)|(knave))" 
+
+        leastExp = r"([A-Z][a-z]*) ((is)|(am)) a ((knight)|(knave)) ((and)|(or)) ([A-Z][a-z]*) ((is)|(am)) a ((knight)|(knave))"
+        result = re.search(leastExp, sentText.replace("I ", speaker + " "))
+        if result:
+            return "%s(%s) %s %s(%s)" % (result.group(5), result.group(1), result.group(8), result.group(15), result.group(11))
 
     def containsBasicStatement(self, sentText):
         baseRegExp = r"([A-Z][a-z]*) ((am)|(is)) a ((knight)|(knave))"
@@ -148,6 +238,7 @@ class SentParser:
 
     #Ted claims, `Zeke could say that I am a knave.' knight(ted) and knave(zeke) OR knave(ted) and knight(zeke)
     #Rosa claims, 'Steve could say that I am a knight.' knight(steve) and knight(rosa) or knave(steve) and knave(rosa)
+    #Ted tells you, `Bart would tell you that I am a knave.'
     def containsCouldSay(self, sentText):
         if "could claim" in sentText or "could say" in sentText or ("would tell you" in sentText and "I would tell you" not in sentText):
             return True
@@ -158,21 +249,37 @@ class SentParser:
         regExp = r"([A-Z][a-z]*) could ((claim)|(say)) that I am a ((knight)|(knave))"
         #either I'm telling the truth and zeke is a knave, or I am lying and Zeke is a knight
         result = re.search(regExp, sentText)
+        
         if result and result.groups(0)[4] == "knave":
-            print("  (knave(%s) and knight(%s) or (knight(%s) and knave(%s)" % (kkLogic.entities[0], kkLogic.entities[1], kkLogic.entities[0], kkLogic.entities[1]))  
+            return "(knave(%s) and knight(%s)) or (knight(%s) and knave(%s))" % (kkLogic.entities[0], kkLogic.entities[1], kkLogic.entities[0], kkLogic.entities[1])  
         elif result and result.groups(0)[4] == "knight":
-            print("  (knight(%s) and knight(%s)) or knave(%s) and knave(%s)" % (kkLogic.entities[0], kkLogic.entities[1], kkLogic.entities[0], kkLogic.entities[1]))
+            return "(knight(%s) and knight(%s)) or (knave(%s) and knave(%s))" % (kkLogic.entities[0], kkLogic.entities[1], kkLogic.entities[0], kkLogic.entities[1])
 
         
-        regExp = r"([A-Z][a-z]*) would ((claim)|(would tell you)) that I am a ((knight)|(knave))"
+        regExp = r"([A-Z][a-z]*) would ((claim)|(tell you)) that I am a ((knight)|(knave))"
         result = re.search(regExp, sentText)
         if result and result.groups(0)[4] == "knave":
-            print("  (knave(%s) and knight(%s) or (knight(%s) and knave(%s)" % (kkLogic.entities[0], kkLogic.entities[1], kkLogic.entities[0], kkLogic.entities[1]))  
+            return "(knave(%s) and knight(%s)) or (knight(%s) and knave(%s))" % (kkLogic.entities[0], kkLogic.entities[1], kkLogic.entities[0], kkLogic.entities[1])  
         elif result and result.groups(0)[4] == "knight":
-            print("  (knight(%s) and knight(%s)) or knave(%s) and knave(%s)" % (kkLogic.entities[0], kkLogic.entities[1], kkLogic.entities[0], kkLogic.entities[1])) 
+            return "(knight(%s) and knight(%s)) or (knave(%s) and knave(%s))" % (kkLogic.entities[0], kkLogic.entities[1], kkLogic.entities[0], kkLogic.entities[1]) 
 
+    #Mel says, `Neither Zoey nor I are knaves.'
+    def containsNor(self, sentText):
+        if "Neither" in sentText and "nor" in sentText:
+            return True
+        else:
+            return False
 
+    def genNor(self, sentText, kkLogic):
+         regExp = r"Neither ([A-Z][a-z]*) nor ([A-Z][a-z]*) are ((knights)|(knaves))"
+         result = re.search(regExp, sentText)
+         role = result.group(3)[:-1]
+         if role == "knight":
+             role = "knave"
+         else:
+             role = "knight"
 
+         return "%s(%s) and %s(%s)" % (role, kkLogic.entities[0], role, kkLogic.entities[1])
     def getBasicStatement(self, sentText, speaker):
         containsNot = self.containsNot(sentText)
         baseRegExp = r"([A-Z][a-z]*) is a ((knight)|(knave))"
@@ -192,13 +299,22 @@ class SentParser:
     def containsConjunction(self, sentText):
         #At least one of the following is true: 
         #Either Ted is a knave or I am a knight.
-        if "least one of the following is true" in sentText or "Both" in sentText or "Either" in sentText:
+        containsAnd = False
+        match2 = False
+        regExp = r"I and ([A-Z][a-z]*) are ((knights)|(knaves))" 
+        regExp2 = r"([A-Z][a-z]*) ((is)|(am)) a ((knight)|(knave)) ((and)|(or)) ([A-Z][a-z]*) ((is)|(am)) a ((knight)|(knave))"
+        if len(re.findall(regExp2, sentText)) > 0:
+            match2 = True
+        #Zed says, `Bart is a knight or I am a knight.
+        if len(re.findall(regExp, sentText)) > 0:
+            containsAnd = True
+        if "least one of the following is true" in sentText or "Both" in sentText or "Either" in sentText or containsAnd or match2:
             return True
         else:
             return False
 
     def containsXor(self, sentText):
-        if "exactly one is a" in sentText or "is different" in sentText or "not the same" in sentText:
+        if "exactly one is a" in sentText or "is different" in sentText or "are different" in sentText or "not the same" in sentText:
             return True
         else:
             return False
@@ -209,6 +325,17 @@ class SentParser:
 
 pInfo = PuzzleInfo()
 sentParser = SentParser()
-for number, question in pInfo.puzzleQuestions.items():
+#nums = sorted(pInfo.puzzleQuestions.keys())
+#for num in nums:
 #    print(question)
-    sentParser.parseLogic(question)
+#    kkLogic = sentParser.parseLogic(pInfo.puzzleQuestions[num])
+#    print(kkLogic.evalKnightKnave())
+if len(sys.argv) > 1:
+    f = open(sys.argv[1])
+    text = f.readlines()
+    for line in text:
+        print(line)
+        kkLogic = sentParser.parseLogic(line)
+        print(kkLogic.evalKnightKnave())
+else:
+    print("please input a valid file name")
